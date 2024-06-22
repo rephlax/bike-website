5e62646eab48f7d45c9a5256ea2aeffb1eacd6be
Luke <45095762+rephlax@users.noreply.github.com>
2024-06-22 21:09:50 +0100

    description list completion

--- src/components/display-area.js
+++ src/components/display-area.js
@@ -20,7 +20,7 @@ class displayArea extends LitElement(){
         html = `
         <div class="content-container">
             <div class="title-container">
-            <h2 class="title">Bike Specifications</h2>
+            <h2 class="title">Bike Specifications for the ${this.bikeData.make} ${this.bikeData.model}</h2>
             </div>
             ${this.bikeData
             ? html`
@@ -28,26 +28,78 @@ class displayArea extends LitElement(){
                 <div class="spec">
                     <h3 class="spec-title">Engine</h3>
                     <dl>
-                    <dt></dt>
-                    <dd></dd>
+                    <dt>Engine</dt>
+                    <dd>${this.bikeData.engine}</dd>
+                    <dt>Displacement</dt>
+                    <dd>${this.bikeData.displacement}</dd>
+                    <dt>Bore x stroke</dt>
+                    <dd>${this.bikeData.bore_stroke}</dd>
+                    <dt>Compression ratio</dt>
+                    <dd>${this.bikeData.compression}</dd>
+                    <dt>Valves</dt>
+                    <dd>${this.bikeData.valves_per_cylinder}</dd>
+                    <dt>Fuel system</dt>
+                    <dd>${this.bikeData.fuel_system}</dd>
+                    <dt>Ignition</dt>
+                    <dd>${this.bikeData.ignition}</dd>
+                    <dt>lubrication</dt>
+                    <dd>${this.bikeData.lubrication}</dd>
                     </dl>
-                    <p>Type: ${this.bikeData.engineType}</p>
-                    <p>Displacement: ${this.bikeData.displacement}</p>
                 </div>
                 <div class="spec">
                     <h3 class="spec-title">Brakes & Suspension</h3>
-                    <p>Length: ${this.bikeData.length}</p>
-                    <p>Width: ${this.bikeData.width}</p>
+                    <dl>
+                    <dt>Front brakes</dt>
+                    <dd>${this.bikeData.front_brakes}</dd>
+                    <dt>Rear brakes</dt>
+                    <dd>${this.bikeData.rear_brakes}</dd>
+                    <dt>Front suspension</dt>
+                    <dd>${this.bikeData.front_suspension}</dd>
+                    <dt>Rear suspension</dt>
+                    <dd>${this.bikeData.rear_suspension}</dd>
+                    </dl>
                 </div>
                 <div class="spec">
                     <h3 class="spec-title">Performance and Transmition</h3>
-                    <p>Power: ${this.bikeData.power}</p>
-                    <p>Torque: ${this.bikeData.torque}</p>
+                    <dl>
+                    <dt>Power</dt>
+                    <dd>${this.bikeData.power}</dd>
+                    <dt>Torque</dt>
+                    <dd>${this.bikeData.torque}</dd>
+                    <dt>Transmission</dt>
+                    <dd>${this.bikeData.transmission}</dd>
+                    <dt>Clutch</dt>
+                    <dd>${this.bikeData.clutch}</dd>
+                    <dt>Gearbox</dt>
+                    <dd>${this.bikeData.gearbox}</dd>
+                    </dl>
                 </div>
                 <div class="spec">
                     <h3 class="spec-title">Frame and Dimensions</h3>
-                    <p>Weight: ${this.bikeData.weight}</p>
-                    <p>Fuel Capacity: ${this.bikeData.fuelCapacity}</p>
+                    <dl>
+                    <dt>Frame</dt>
+                    <dd>${this.bikeData.frame}</dd>
+                    <dt>Front wheel travel</dt>
+                    <dd>${this.bikeData.front_wheel_travel}</dd>
+                    <dt>Rear wheel travel</dt>
+                    <dd>${this.bikeData.rear_wheel_travel}</dd>
+                    <dt>Front tyre</dt>
+                    <dd>${this.bikeData.front_tire}</dd>
+                    <dt>Rear tyre</dt>
+                    <dd>${this.bikeData.rear_tire}</dd>
+                    <dt>Seat height</dt>
+                    <dd>${this.bikeData.seat_height}</dd>
+                    <dt>Total height</dt>
+                    <dd>${this.bikeData.total_height}</dd>
+                    <dt>Total length</dt>
+                    <dd>${this.bikeData.total_length}</dd>
+                    <dt>Ground clearance</dt>
+                    <dd>${this.bikeData.ground_clearance}</dd>
+                    <dt>Fuel capacity</dt>
+                    <dd>${this.bikeData.fuel_capacity}</dd>
+                    <dt>Total weight</dt>
+                    <dd>${this.bikeData.total_weight}</dd>
+                    </dl>
                 </div>
                 </div>
           ` : html`<p>Select a bike to see the details.</p>`}
