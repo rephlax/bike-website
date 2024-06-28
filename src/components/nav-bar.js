4efaf00087f06ebaed1460c6a983fec6432ac57d
Luke <45095762+rephlax@users.noreply.github.com>
2024-06-28 17:55:17 +0100

    main import and styling nav
    +import of nav bar to html and main js file
    + styling of nav bar (basic before themes)

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -17,16 +17,46 @@ class NavBar extends LitElement {
     static get styles() {
         return css `
             :host {
-            display: block;
-            width: 100%;
-            background-color: #333;
-            color: white;
-            padding: 10px 0;
+                display: block;
+                width: 100%;
+                background-color: #333;
+                color: white;
+                padding: 10px 0;
             }
 
             .navbar {
-            
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 0 20px;
             }
+
+            .navbar svg {
+                width:
+                height:
+                fill: white;
+            }
+
+            .navbar button:disabled {
+                color: gray;
+                cursor: not-allowed;
+            }
+
+            .navbar a {
+                color: white;
+                text-decoration: none;
+            }
+
+            @media (max-width: 37.5rem) {
+                .navbar {
+                flex-direction: column;
+                gap: 10px;
+            }
+                .navbar button {
+                font-size: 14px;
+                margin-left: 0;
+            }
+        }
     `};
 
     render(){
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import './components/search-button.js';
 import './components/search-form.js';
 import './components/selection-screen.js';
 import './components/display-area.js';
+import './components/nav-bar.js';
 // Ensure other imports are correctly pointed to their respective paths
