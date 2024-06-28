a577b9959cf67ea4521112cfb84f0de2c13ed0ec
Luke <45095762+rephlax@users.noreply.github.com>
2024-06-28 19:12:42 +0100

    more styling on button for manufacturer web and html
    + html for navbar and styling on button for manufacturer website

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -47,6 +47,19 @@ class NavBar extends LitElement {
                 text-decoration: none;
             }
 
+            .navbar button {
+                background: none;
+                border: none;
+                color: white;
+                cursor: pointer;
+                font-size: 16px;
+                margin-left: 20px;
+            }
+            .navbar button:disabled {
+                color: gray;
+                cursor: not-allowed;
+            }
+
             @media (max-width: 37.5rem) {
                 .navbar {
                 flex-direction: column;
@@ -61,7 +74,17 @@ class NavBar extends LitElement {
 
     render(){
        return html = `
-        
+        <div class="navbar">
+            <a href="#" @click=${this._goHome}>
+                <svg viewBox="0 0 64 64">
+                    <path d="M32 0C14.327 0 0 14.327 0 32s14.327 32 32 32 32-14.327 32-32S49.673 0 32 0zm0 60C16.561 60 4 47.439 4 32S16.561 4 32 4s28 12.561 28 28-12.561 28-28 28z"/>
+                    <path d="M32 14c-9.941 0-18 8.059-18 18s8.059 18 18 18 18-8.059 18-18-8.059-18-18-18zm0 32c-7.732 0-14-6.268-14-14s6.268-14 14-14 14 6.268 14 14-6.268 14-14 14z"/>
+                    <path d="M32 22c-5.514 0-10 4.486-10 10s4.486 10 10 10 10-4.486 10-10-4.486-10-10-10zm0 16c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/>
+                </svg>
+            </a>
+        <slot name="search-button"></slot>
+        <button ?disabled=${!this.bikeSelected} @click=${this._goToManufacturer}>Manufacturer</button>
+        </div>
         `
     }
 }
