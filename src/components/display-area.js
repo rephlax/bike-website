93635f3e25b6c59843567c13359abe6363b65091
Luke <45095762+rephlax@users.noreply.github.com>
2024-08-07 19:36:16 +0100

    another fix on the code

--- src/components/display-area.js
+++ src/components/display-area.js
@@ -29,7 +29,7 @@ class DisplayArea extends LitElement {
         padding: 16px;
         border-radius: 8px;
         background-color: var(--background-color);
-        margin: 0 50px 25px; /* Margins on either side for spacing */
+        margin: 0 50px 50px 25px; /* Margins on either side for spacing */
         font-weight: 400;
         line-height: 1.6;
         font-size: 1rem;
